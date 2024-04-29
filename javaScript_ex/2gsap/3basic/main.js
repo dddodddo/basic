9d54433ac4104af1f3e307f0fece75dee5abc397
flex-site <99pdh@naver.com>
2024-04-29 17:49:02 +0900

    4.29/7

--- javaScript_ex/2gsap/3basic/main.js
+++ javaScript_ex/2gsap/3basic/main.js
@@ -1,1 +1,28 @@
 gsap.registerPlugin(ScrollTrigger)
+
+//visual
+gsap.timeline({
+    scrollTrigger:{
+        trigger:".visual",
+        start:"top top",
+        end:"bottom top",
+        scrub:2,
+        pin:true,
+    }
+})
+.to(".visual h1",{opacity:1,duration:10},5)
+.to(".visual img",{scale:0.4, opacity:0.3,duration:10},5)
+
+let imgBox=document.querySelectorAll('.imgBox')
+// console.log(imgBox)
+
+// 배열형태로 불러줌 --> gsap명령문
+// console.log(gsap.utils.toArray('.imgBox'))
+
+imgBox.forEach(function(imgBox){
+    gsap.timeline({
+        scrollTrigger:{
+            
+        }
+    })
+})
