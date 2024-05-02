acd871f0d59a691882cc3879db5b8ac4b7da00b1
flex-site <99pdh@naver.com>
2024-05-02 14:10:37 +0900

    5.2

--- 연습/포트폴리오/pf_01/js/main.js
+++ 연습/포트폴리오/pf_01/js/main.js
@@ -1,1 +1,20 @@
-gsap.to(".green", {y: -100, duration: 1});
+
+
+let tl=gsap.timeline()
+
+tl.to(".topimg1", {
+    y:[-100,0],
+    ease: "elastic.out(1,0.3)",
+    duration: 3,
+    stagger:0.2,
+    
+})
+
+gsap.to(".topimg1", {
+    keyframes: {
+      
+      y: [0, 0, 100, 100, 0],
+      ease: "elastic.out(1,0.3)",
+    },
+    duration: 2
+  });
