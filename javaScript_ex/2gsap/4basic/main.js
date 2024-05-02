c562243619998ae43143ea3a472f6530eac0b7c7
flex-site <99pdh@naver.com>
2024-05-02 17:50:17 +0900

    5.2/8

--- javaScript_ex/2gsap/4basic/main.js
+++ javaScript_ex/2gsap/4basic/main.js
@@ -1,1 +1,8 @@
 gsap.registerPlugin(ScrollTrigger)
+
+gsap.from(".visual .subtitle",{
+    y:50 ,opacity:0, ease:"expo.out", duration:1, delay:0.5
+})
+gsap.from(".visual .text",{
+    y:50 ,opacity:0, ease:"expo.out", duration:1, delay:1
+})
--- 연습/포트폴리오/pf_01/js/main.js
+++ 연습/포트폴리오/pf_01/js/main.js
@@ -1,20 +1,4 @@
 
-
-let tl=gsap.timeline()
-
-tl.to(".topimg1", {
-    y:[-100,0],
-    ease: "elastic.out(1,0.3)",
-    duration: 3,
-    stagger:0.2,
-    
+gsap.from(".topimg1",{
+  y:50 ,opacity:0, ease:"expo.out", duration:1, delay:0.5
 })
-
-gsap.to(".topimg1", {
-    keyframes: {
-      
-      y: [0, 0, 100, 100, 0],
-      ease: "elastic.out(1,0.3)",
-    },
-    duration: 2
-  });
