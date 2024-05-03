e6931e4c34e9a8a52ba109fc7761c3e9685b0ced
flex-site <99pdh@naver.com>
2024-05-03 16:57:16 +0900

    5.3/7

--- javaScript_ex/2gsap/4basic/main.js
+++ javaScript_ex/2gsap/4basic/main.js
@@ -6,3 +6,55 @@ gsap.from(".visual .subtitle",{
 gsap.from(".visual .text",{
     y:50 ,opacity:0, ease:"expo.out", duration:1, delay:1
 })
+
+// slide
+let list=document.querySelectorAll(".work ul li")
+console.log(list)
+let imgBoxs=document.querySelectorAll(".imgBox")
+console.log(imgBoxs)
+
+//가로 스크롤 
+let scrollTween=gsap.to(list,{
+    xPercent:-100*(list.length - 1),
+    ease:"none",
+    scrollTrigger:{
+        trigger:".work",
+        start:"center center",
+        scrub:1,
+        end:"+=300%",
+        pin:true
+    }
+})
+
+//배열안에 요소를 하나씩 가져와서 어떤 일을 시킨다
+imgBoxs.forEach(function(imgBox){//item은 배열안에 각각요소를 순서대로 받는다
+
+
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:imgBox,
+            start:"center right",
+            end:'center center',
+            containerAnimation:scrollTween,
+            scrub:true,
+            markers:true
+        }
+    })
+    .to(imgBox,{'clip-path':'inset(0%)',ease:"none",duration:1},0)
+    
+    
+    
+    
+    //왼쪽으로 사라질때 이미지를 작게 
+    gsap.timeline({
+        scrollTrigger:{
+            trigger:imgBox,
+            start:"center center",
+            end:'center left',
+            containerAnimation:scrollTween,
+            scrub:true,
+            markers:true
+        }
+    })
+    .to(imgBox,{'clip-path':'inset(30%)',ease:"none",duration:1},0)
+})
