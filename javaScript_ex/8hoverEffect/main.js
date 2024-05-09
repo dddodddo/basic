c0f99f216a7ef59fd78e317f08be6cb961fbee1e
flex-site <99pdh@naver.com>
2024-05-09 14:47:57 +0900

    5.9/5

--- javaScript_ex/8hoverEffect/main.js
+++ javaScript_ex/8hoverEffect/main.js
@@ -19,10 +19,40 @@ menuItem.forEach(function(item){
     })
 })
 
-let appendImages=function(src){}
+function removeExtraImages(container){
+    while(container.children.length>10){//이미지의 갯수가 10보다 커지면
+        container.removeChild(container.firstChild)//첫번째 자식요소를 지운다.
+    }
+}
+
+let appendImages=function(src){
+    let preview1=document.querySelector(".preview-img-1")
+    let preview2=document.querySelector(".preview-img-2")
+
+    let img1=document.createElement("img")
+    let img2=document.createElement("img")
+
+    img1.src=src
+    img2.src=src
+    console.log(img1)
+    img1.style.clipPath="polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)"
+    img2.style.clipPath="polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)"
+
+    preview1.appendChild(img1)
+    preview2.appendChild(img2)
+
+    gsap.to([img1,img2],{
+        clipPath:"polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+        duration:1,
+        onComplete:function(){//gsap 애니메이션이 끝나고 나서 할 일
+            removeExtraImages(preview1)
+            removeExtraImages(preview2)
+        }
+    })
+}
 
 let mouseoverAnimation=function(elem){
-    console.log(elem)
+    // console.log(elem)
     gsap.to(elem.querySelectorAll("p:nth-child(1)"),{
         top:"-100%",
         duration:0.3
@@ -47,6 +77,7 @@ document.querySelectorAll(".menu-item").forEach(function(item,index){
     item.addEventListener("mouseover",function(){
         // console.log("실행")
         mouseoverAnimation(item)
+        appendImages(imageSources[index])
     })
     item.addEventListener("mouseout",function(){
         // console.log("실행")
@@ -55,6 +86,6 @@ document.querySelectorAll(".menu-item").forEach(function(item,index){
 
 })
 
-appendImages()
+
 
 
