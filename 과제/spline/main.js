85f511d995d44f19d48e18bbe27885bdc92bbf7f
flex-site <99pdh@naver.com>
2024-05-14 17:51:03 +0900

    5.14/*

---
+++ 과제/spline/main.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger)
+function lenis(){
+    const lenis = new Lenis()
+    
+    lenis.on('scroll', (e) => {
+      // console.log(e)
+    })
+    
+    lenis.on('scroll', ScrollTrigger.update)
+    
+    gsap.ticker.add((time)=>{
+      lenis.raf(time * 1000)
+    })
+    
+    gsap.ticker.lagSmoothing(0)
+
+}
+lenis()
+
+import { Application } from 'https://unpkg.com/@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app
+.load('https://prod.spline.design/lGbFqJJNNEymupwX/scene.splinecode')
+.then(()=>{
+  
+})
