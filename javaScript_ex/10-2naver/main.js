a122d61f867775edb6ddff5a00b5d25604826837
flex-site <99pdh@naver.com>
2024-05-17 15:01:46 +0900

    5.17/6

--- javaScript_ex/10-2naver/main.js
+++ javaScript_ex/10-2naver/main.js
@@ -1,25 +1,55 @@
-let id=document.querySelector("#id")
-let error=document.querySelectorAll(".error_next_box")
+let id = document.querySelector("#id")
+let error = document.querySelectorAll(".error_next_box")
 console.log(error)
+
+let pw1 = document.querySelector('#psw1')
+let pwImg1 = document.querySelector('#psw1_img1')
+let pwMsg = document.querySelector('#alertTxt')
+
 // id.addEventListener('focusout',function(){
 //     checkId()
 // })
-id.addEventListener('focusout',checkId)
+id.addEventListener('focusout', checkId)
+pw1.addEventListener('focusout', checkPw)
 
-function checkId(){
-    let idPattern=/^[a-zA-Z0-9_-]{5,20}$/
-    if(id.value===""){//id가 비어있다면
-        error[0].innerHTML="필수 정보입니다."
-        error[0].style.display="block"
-        error[0].style.color="#f00"
+function checkId() {
+    let idPattern = /^[a-zA-Z0-9_-]{5,20}$/
+    if (id.value === "") { //id가 비어있다면
+        error[0].innerHTML = "필수 정보입니다."
+        error[0].style.display = "block"
+        error[0].style.color = "#f00"
 
-    }else if(!idPattern.test(id.value)){ //test --> 정규식 확인하는 함수
-        error[0].innerHTML="5~20자의 영문 소문자, 대문자, 숫자와 특수기호(_),(-)만 사용가능합니다."
-        error[0].style.display="block"
-        error[0].style.color="#f00"
+    } else if (!idPattern.test(id.value)) { //test --> 정규식 확인하는 함수
+        error[0].innerHTML = "5~20자의 영문 소문자, 대문자, 숫자와 특수기호(_),(-)만 사용가능합니다."
+        error[0].style.display = "block"
+        error[0].style.color = "#f00"
+    } else {
+        error[0].innerHTML = "멋진 아이디입니다."
+        error[0].style.display = "block"
+        error[0].style.color = "#80a600"
+    }
+}
+
+function checkPw() {
+    let pwPattern = /^[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}$/
+    console.log(pwPattern.test(pw1.value))
+    if (pw1.value === "") {
+        error[1].innerHTML = `필수 정보입니다.`
+        error[1].style.display = "block"
+        pwMsg[1].style.display = "none"
+        pwImg1.src="img/m_icon_not_use.png"
+    }else if(!pwPattern.test(pw1.value)){
+        error[1].innerHTML = `8~16자 영문, 대소문자, 숫자, 특수문자를 사용하세요`
+        error[1].style.display = "block"
+        pwMsg.innerHTML= "사용불가"
+        pwMsg.style.display= "block"
+        pwMsg.style.color= "#f00"
+        pwImg1.src="img/m_icon_not_use.png"
     }else{
-        error[0].innerHTML="멋진 아이디입니다."
-        error[0].style.display="block"
-        error[0].style.color="#80a600"
+        error[1].style.display = "none"
+        pwMsg.innerHTML= "안전"
+        pwMsg.style.display= "block"
+        pwMsg.style.color= "#03c75a"
+        pwImg1.src="img/m_icon_safe.png"
     }
 }
--- javaScript_ex/13imgGallery_3D/main.js
+++ javaScript_ex/13imgGallery_3D/main.js
@@ -6,10 +6,38 @@ window.onload=function(){
     galleryWrap.addEventListener("mousemove",function(event){
         let x=event.clientX
         let y=event.clientY
-        console.log(`${x},${y}`)
+        // console.log(`${x},${y}`)
         let centerX=window.innerWidth / 2 //window.innerWidth 화면의 넓이
         let centerY=window.innerHeight / 2 //window.innerHeight 화면의 높이
 
         let percentX=(x - centerX)/centerX
     })
+
+    for(let i=0; i<150; i++){
+        let item=document.createElement("div")
+        item.className="item"
+        let img=document.createElement('img')
+        img.src='./assets/img' + ((i % 15) + 1) + ".jpg"
+        // console.log(item)
+        item.appendChild(img)
+        gallery.appendChild(item)
+        // console.log(gallery)
+    }
+
+    let items=document.querySelectorAll(".item")
+    let numberOfItems=items.length
+    let angleIncrement=360 / numberOfItems
+    console.log(angleIncrement)
+
+    // //각각의 요소 item, 번호 index
+    // items.forEach(function(item, index){
+    //     gsap.set(item,{
+    //         rotationY:90,
+    //         rotationZ:index * angleIncrement - 90,
+    //         transformOrigin:"50% 400px"
+    //     })
+    // })
+
+
+
 }
