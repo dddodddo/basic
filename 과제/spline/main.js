8fac77be5a8d29e38e53f1eeb5b135a88f53ec13
dddodddo <99pdh@naver.com>
2024-05-18 18:48:49 +0900

    .

--- 과제/spline/main.js
+++ 과제/spline/main.js
@@ -1,28 +1,28 @@
-gsap.registerPlugin(ScrollTrigger)
-function lenis(){
-    const lenis = new Lenis()
+// gsap.registerPlugin(ScrollTrigger)
+// function lenis(){
+//     const lenis = new Lenis()
     
-    lenis.on('scroll', (e) => {
-      // console.log(e)
-    })
+//     lenis.on('scroll', (e) => {
+//       // console.log(e)
+//     })
     
-    lenis.on('scroll', ScrollTrigger.update)
+//     lenis.on('scroll', ScrollTrigger.update)
     
-    gsap.ticker.add((time)=>{
-      lenis.raf(time * 1000)
-    })
+//     gsap.ticker.add((time)=>{
+//       lenis.raf(time * 1000)
+//     })
     
-    gsap.ticker.lagSmoothing(0)
+//     gsap.ticker.lagSmoothing(0)
 
-}
-lenis()
+// }
+// lenis()
 
-import { Application } from 'https://unpkg.com/@splinetool/runtime';
+// import { Application } from 'https://unpkg.com/@splinetool/runtime';
 
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app
-.load('https://prod.spline.design/lGbFqJJNNEymupwX/scene.splinecode')
-.then(()=>{
+// const canvas = document.getElementById('canvas3d');
+// const app = new Application(canvas);
+// app
+// .load('https://prod.spline.design/lGbFqJJNNEymupwX/scene.splinecode')
+// .then(()=>{
   
-})
+// })
