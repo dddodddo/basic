a23a0940c810852bd2f0f24324929a691e13b33f
dddodddo <99pdh@naver.com>
2024-05-19 16:25:02 +0900

    5.19/**

--- 과제/spline/main.js
+++ 과제/spline/main.js
@@ -1,26 +1,15 @@
-
-// import { Application } from 'https://unpkg.com/@splinetool/runtime';
-
-// const canvas = document.getElementById('canvas3d');
-// const app = new Application(canvas);
-// app
-// .load('https://prod.spline.design/lGbFqJJNNEymupwX/scene.splinecode')
-// .then(()=>{
-  
-// })
-
 gsap.registerPlugin(ScrollTrigger)
 
 
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
-  console.log(e)
+  // console.log(e)
 })
 
 lenis.on('scroll', ScrollTrigger.update)
 
-gsap.ticker.add((time)=>{
+gsap.ticker.add((time) => {
   lenis.raf(time * 1000)
 })
 
@@ -28,12 +17,12 @@ gsap.ticker.lagSmoothing(0)
 
 
 /////////////////////////////////
-gsap.to("#section1",{
-  scrollTrigger:{
-    trigger:'#section1',
-    start:"top",
-    end:"bottom",
-    scrub:1,
-    markers:true,
-  }
-})
+gsap.to("#section1", {
+  // scrollTrigger:{
+  //   trigger:"#section1",
+  //   start:"top 10%",
+  //   end:"bottom 20%",
+  //   marker:true,
+  //   scrub:1
+  // }
+});
