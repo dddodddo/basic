9d6a1f80e9b1ad6276af0b4681abf1a174df56b4
flex-site <99pdh@naver.com>
2024-05-21 16:51:54 +0900

    5.21/7

--- javaScript_ex/2gsap/6basic_party/main.js
+++ javaScript_ex/2gsap/6basic_party/main.js
@@ -23,13 +23,6 @@ gsap.to('.badge',{
 })
 
 ///////////////////////////////////
-
-// 첫번째 영역
-let sticky=document.querySelector('.sticky')
-gsap.to(sticky,{
-
-})
-
 //얼굴
 let wrapper=document.querySelector('.tracker')
 let emoji=document.querySelector('.emoji')
@@ -38,7 +31,75 @@ let emojiFace=document.querySelector('.emoji-face')
 let moveEvent=function(e){
     let wrapperRect=wrapper.getBoundingClientRect() //wrapper에 대한 영역(공간) 정보
     console.log(wrapperRect)
+
+    let relX=e.clientX - (wrapperRect.left + wrapperRect.width/2)
+    let relY=e.clientY - (wrapperRect.top + wrapperRect.height/2)
+
+    let emojiMaxDisplacement = 50
+    let emojiFaceMaxDisplacement = 75
+
+    let emojiFaceDisplacementX =(relX / wrapperRect.width) * emojiFaceMaxDisplacement
+    let emojiFaceDisplacementY =(relY / wrapperRect.height) * emojiFaceMaxDisplacement
+
+    let emojiMaxDisplacementX=(relX / wrapperRect.width) *emojiMaxDisplacement
+    let emojiMaxDisplacementY=(relY / wrapperRect.height) *emojiMaxDisplacement
+
+    gsap.to(emojiFace,{
+      x:emojiFaceDisplacementX,
+      y:emojiFaceDisplacementY,
+      duration:0.35,
+      ease:"power3.out"
+    })
+    gsap.to(emoji,{
+      x:emojiMaxDisplacementX,
+      y:emojiMaxDisplacementY,
+      duration:0.35,
+      ease:"power3.out"
+    })
 }
+
+// 첫번째 영역
+let sticky=document.querySelector('.sticky')
+
+gsap.to(sticky,{
+  scrollTrigger:{
+    trigger: sticky,
+    start:"top top",
+    end:"+=150%",
+    scrub:1,
+  },
+  y:250,
+  scale:0.75,
+  rotation:-15,
+  ease:"power3.out"
+})
+
+// 두번째 영역
+let conScale=document.querySelector('.con-scale')
+
+gsap.fromTo(conScale,{
+  y:100,
+  scale:0.7,
+  rotation:15
+},{
+  scrollTrigger:{
+    trigger:conScale,
+    start:"top 80%",
+    end:"top 20%",
+    scrub:2
+  },
+  y:0,
+  scale:1,
+  duration:1,
+  rotation:0,
+  ease:"power3.out"
+})
+
+
+
+
+
 
 // wrapper.addEventListener('mousemove',function(){moveEvent()})
 wrapper.addEventListener('mousemove',moveEvent)
+
