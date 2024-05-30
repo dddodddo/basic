66f279e0fd0dabd434c3a33b67a8af3d943e7509
dddodddo <99pdh@naver.com>
2024-05-30 21:49:08 +0900

    5.30/**

--- 연습/자격증/Green복지재단(A2)/js/script.js
+++ 연습/자격증/Green복지재단(A2)/js/script.js
@@ -26,19 +26,16 @@ $(function(){
     },3000)
 })
 
-// $(function(){
-//     let currentIndex = 0;
-//     $(".sliderWrap").append($(".slider").first().clone(true));
+$(".popup-btn").click(function(){
+    $(".popup-view").show()
+})
+$(".popup-close").click(function(){
+    $(".popup-view").hide()
+})
 
-//     setInterval(function(){ 
-//         currentIndex++;  
-//         $(".sliderWrap").animate({marginLeft: -currentIndex * 100 + "%"}, 600); 
-
-//         if(currentIndex == 3){ 
-//             setTimeout(function(){  
-//                 $(".sliderWrap").animate({marginLeft: 0}, 0);  
-//                 currentIndex = 0;   
-//             }, 700);
-//         }
-//     }, 3000);
-// });
+$(".popup-btn").click(function(){
+    $(".popup-view").show();
+});
+$(".popup-close").click(function(){
+    $(".popup-view").hide ();
+});
