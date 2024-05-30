f9d213a2bf361d2fc497604a1b690fb6d9b8d336
flex-site <99pdh@naver.com>
2024-05-30 17:49:51 +0900

    5.30/8

--- 연습/자격증/Green복지재단(A2)/js/script.js
+++ 연습/자격증/Green복지재단(A2)/js/script.js
@@ -8,3 +8,37 @@ $(function(){
         $('#header').removeClass("on")
     })
 })
+
+$(function(){
+    let currentIndex=0
+    $(".sliderWrap").append($(".slider").first().clone(true))
+    
+    setInterval(function(){
+        currentIndex++
+        $(".sliderWrap").animate({marginLeft:-currentIndex * 100 + "%"},600)
+        
+        if(currentIndex == 3){
+            setTimeout(function(){
+                $(".sliderWrap").animate({marginLeft:0},0)
+                currentIndex=0
+            },700)
+        }
+    },3000)
+})
+
+// $(function(){
+//     let currentIndex = 0;
+//     $(".sliderWrap").append($(".slider").first().clone(true));
+
+//     setInterval(function(){ 
+//         currentIndex++;  
+//         $(".sliderWrap").animate({marginLeft: -currentIndex * 100 + "%"}, 600); 
+
+//         if(currentIndex == 3){ 
+//             setTimeout(function(){  
+//                 $(".sliderWrap").animate({marginLeft: 0}, 0);  
+//                 currentIndex = 0;   
+//             }, 700);
+//         }
+//     }, 3000);
+// });
