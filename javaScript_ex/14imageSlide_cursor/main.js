3fed1d770fdc44cfa89dd3dd24c8e476ccf6d934
flex-site <99pdh@naver.com>
2024-05-31 15:37:06 +0900

    5.31/6

--- javaScript_ex/14imageSlide_cursor/main.js
+++ javaScript_ex/14imageSlide_cursor/main.js
@@ -1,75 +1,84 @@
-let imageSliders=document.querySelectorAll(".image-slider")
+let imageSliders = document.querySelectorAll(".image-slider")
 //[item,item]
-imageSliders.forEach(function(imageSlider){
-   let prev=imageSlider.querySelector(".prev-slide");
-   let next=imageSlider.querySelector(".next-slide");
-   let slideNum=imageSlider.querySelector(".slide-num");
-   let tracker=imageSlider.querySelector(".tracker");
-   let images=imageSlider.querySelectorAll(".image-slider img");
+imageSliders.forEach(function (imageSlider) {
+  let prev = imageSlider.querySelector(".prev-slide");
+  let next = imageSlider.querySelector(".next-slide");
+  let slideNum = imageSlider.querySelector(".slide-num");
+  let tracker = imageSlider.querySelector(".tracker");
+  let images = imageSlider.querySelectorAll(".image-slider img");
 
 
-    let slideId=0 //슬라이드 번호
-    prev.addEventListener("click",function(){
-        slideId --
-        if(slideId<0){
-            slideId = images.length -1
-        }
-        slideNum.innerHTML=`${slideId + 1} / ${images.length}`
-
-        //모든 이미지 선택
-        images.forEach(function(img){
-            img.style.width=0
-            img.style.right=0
-            img.style.left="initial"
-        })
-        images[slideId].style.width="100%"
-        images[slideId].style.right="initial"
-        images[slideId].style.left="0"
-    })//click함수
-
-    next.addEventListener("click",function(){
-        slideId ++
-        if(slideId>images.length - 1){
-            slideId = 0
-        }
-        slideNum.innerHTML=`${slideId + 1} / ${images.length}`
-
-        //모든 이미지 선택
-        images.forEach(function(img){
-            img.style.width=0
-            img.style.left=0
-            img.style.right="initial"
-        })
-        images[slideId].style.width="100%"
-        images[slideId].style.left="initial"
-        images[slideId].style.right="0"
-    })//click함수
-
-    let scrollAmmount=0
-    let yPos=imageSlider.offsetTop// 문서의 top에서 선택한 영역의 top까지의 높이값
-    // console.log(yPos) 477 1906
-    let xPos=0
-
-    function mouseTracker(){
-        scrollAmmount=window.scrollY + yPos
-
-        tracker.style.top=`${scrollAmmount}px`
-        tracker.style.left=`${xPos}px`
+  let slideId = 0 //슬라이드 번호
+  prev.addEventListener("click", function () {
+    slideId--
+    if (slideId < 0) {
+      slideId = images.length - 1
     }
+    slideNum.innerHTML = `${slideId + 1} / ${images.length}`
 
-    window.addEventListener("mousemove",function(e){
-        yPos=(e.clientY - tracker.offsetHeight/2) - imageSlider.offsetTop
-        xPos=(e.clientX - tracker.offsetWidth/2) - imageSlider.offsetLeft
-        mouseTracker()
+    //모든 이미지 선택
+    images.forEach(function (img) {
+      img.style.width = 0
+      img.style.right = 0
+      img.style.left = "initial"
     })
+    images[slideId].style.width = "100%"
+    images[slideId].style.right = "initial"
+    images[slideId].style.left = "0"
+  }) //click함수
 
-    window.addEventListener("scroll",function(){
-        mouseTracker()
+  next.addEventListener("click", function () {
+    slideId++
+    if (slideId > images.length - 1) {
+      slideId = 0
+    }
+    slideNum.innerHTML = `${slideId + 1} / ${images.length}`
+
+    //모든 이미지 선택
+    images.forEach(function (img) {
+      img.style.width = 0
+      img.style.left = 0
+      img.style.right = "initial"
     })
+    images[slideId].style.width = "100%"
+    images[slideId].style.left = "initial"
+    images[slideId].style.right = "0"
+  }) //click함수
 
-    prev.addEventListener("mouseenter",function(){
-        imageSlider.querySelector(".arrow-prev")
-    })
+  let scrollAmmount = 0
+  let yPos = imageSlider.offsetTop // 문서의 top에서 선택한 영역의 top까지의 높이값
+  // console.log(yPos) 477 1906
+  let xPos = 0
 
+  function mouseTracker() {
+    scrollAmmount = window.scrollY + yPos
 
-})//forEach문
+    tracker.style.top = `${scrollAmmount}px`
+    tracker.style.left = `${xPos}px`
+  }
+
+  window.addEventListener("mousemove", function (e) {
+    yPos = (e.clientY - tracker.offsetHeight / 2) - imageSlider.offsetTop
+    xPos = (e.clientX - tracker.offsetWidth / 2) - imageSlider.offsetLeft
+    mouseTracker()
+  })
+
+  window.addEventListener("scroll", function () {
+    mouseTracker()
+  })
+
+  prev.addEventListener("mouseenter", function () {
+    imageSlider.querySelector(".arrow-prev").style.width = "1rem"
+    imageSlider.querySelector(".arrow-next").style.width = "0"
+    tracker.style.paddingRight = 0
+  })
+  next.addEventListener("mouseenter", function () {
+    imageSlider.querySelector(".arrow-prev").style.width = "0"
+    imageSlider.querySelector(".arrow-next").style.width = "1rem"
+    tracker.style.paddingLeft = 0
+  })
+
+  prev.addEventListener("mouseout",()=>{tracker.style.paddingRight="0.8em"})
+  next.addEventListener("mouseout",()=>{tracker.style.paddingLeft="0.8em"})
+
+}) //forEach문
--- 연습/자격증/강원천문대(A3)/js/script.js
+++ 연습/자격증/강원천문대(A3)/js/script.js
@@ -6,3 +6,29 @@ $(function(){
         $(this).find(".submenu").stop().slideUp(200)
     })
 })
+
+
+$(function(){
+    let currentImage=0;
+    $(".slider").hide().first().show()
+
+    setInterval(function(){
+        let nextImgae=(currentImage + 1) % 3
+
+        $(".slider").eq(currentImage).fadeOut(1200)
+        $(".slider").eq(nextImgae).fadeIn(1200)
+
+        currentImage=nextImgae
+    },3000)
+})
+
+const tabBtn=$(".info-menu>a")
+const tabCont=$(".info-cont>div")
+tabCont.hide().first().show()
+
+tabBtn.on("click",function(){
+    const Index = $(this).index()
+console.log(Index)
+    $(this).addClass("active").show().siblings().removeClass("active")
+    tabCont.eq(Index).show().siblings().hide()
+})
