00450ddf39605c1518f6d99cf26aa83c3dc7f4ee
flex-site <99pdh@naver.com>
2024-06-03 10:50:30 +0900

    06.03/2

--- web/sushi/main.js
+++ web/sushi/main.js
@@ -18,11 +18,65 @@ let themeButton=document.querySelector("#change-theme")
 let iconTheme="ri-sun-line"// ri-apps-line
 let darkTheme="dark-theme"
 let getCurrentTheme=()=>{
-    return "메롱"
+    //.classList.contains(클래스명) --> 클래스명을 가지고 있는가 (true/false)
+    let result= document.body.classList.contains(darkTheme)?"dark":"light";
+   return result;
+}
+let getCurrentIcon=()=>{
+    let result=themeButton.classList.contains(iconTheme)?"ri-apps-line":"ri-sun-line"
+    return result
 }
 //웹 스토어에 값 셋팅
-//localStorage.setItem() ==> 웹스토어에 값을 입력하는 방법
-//localStorage.getItem() ==> 웹스토어에 값을 가져오는 방법
+//localStorage.setItem(키:값) ==> 웹스토어에 값을 입력하는 방법
+//localStorage.getItem(키) ==> 웹스토어에 값을 가져오는 방법
 themeButton.addEventListener("click",()=>{
+    //toggle 키 --> 실행과 반실행
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
     localStorage.setItem("selected-theme",getCurrentTheme())
+    localStorage.setItem("selected-icon",getCurrentIcon())
 })
+
+
+let selectedTheme=localStorage.getItem("selected-theme")
+let selectedIcon=localStorage.getItem("selected-icon")
+console.log(selectedTheme)
+
+if(selectedTheme){
+    if(selectedTheme == "dark"){
+        document.body.classList.add(darkTheme)
+    }else{
+        document.body.classList.remove(darkTheme)
+    }
+    if(selectedIcon == "ri-apps-line"){
+        themeButton.classList.add(iconTheme)
+    }else{
+        themeButton.classList.remove(iconTheme)
+    }
+}
+
+
+//모바일에서 menu부분
+let navToggle=document.querySelector("#nav_toggle")
+let navMenu=document.querySelector("#nav_menu")
+let navClose=document.querySelector("#nav_close")
+
+navToggle.addEventListener("click",function(){
+    navMenu.classList.add("show-menu")
+})
+navClose.addEventListener("click",function(){
+    navMenu.classList.remove("show-menu")
+})
+
+
+//scrollup
+let scrollup=()=>{
+    // let scrollY=scrollY
+
+    console.log(pageYOffset)
+    let scrollup=document.querySelector("#scroll-up")
+    pageYOffset>=100?
+    scrollup.classList.add("show-scroll"):scrollup.classList.remove("show-scroll")
+}
+
+window.addEventListener("scroll",scrollup)
