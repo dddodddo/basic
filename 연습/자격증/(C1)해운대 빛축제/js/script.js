bc361b0178225a32e25248130cbe545b9859ff81
flex-site <99pdh@naver.com>
2024-06-03 17:33:45 +0900

    06.03/8

--- 연습/자격증/(C1)해운대 빛축제/js/script.js
+++ 연습/자격증/(C1)해운대 빛축제/js/script.js
@@ -2,4 +2,7 @@ $(function(){
     $(".nav>ul>li").mouseover(function(){
         $(this).find($(".submenu")).stop().slideDown()
     })
+    $(".nav>ul>li").mouseout(function(){
+        $(this).find($(".submenu")).stop().slideUp()
+    })
 })
