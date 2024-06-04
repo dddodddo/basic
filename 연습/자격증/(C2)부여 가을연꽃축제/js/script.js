c964299bbe6c6ee64c8cb00d41dc54b779f166c6
flex-site <99pdh@naver.com>
2024-06-04 12:49:17 +0900

    06.04/4

---
+++ 연습/자격증/(C2)부여 가을연꽃축제/js/script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $("nav>ul>li").mouseover(function(){
+        $(this).find($(".submenu")).stop().slideDown(500)
+    })
+    $("nav>ul>li").mouseout(function(){
+        $(this).find($(".submenu")).stop().slideUp(500)
+    })
+})
+
+
+// $(function(){
+//     let currentIndex=0
+//     $(".slider").hide().first().show()
+    
+//     setInterval(function(){
+//         let nextIndex=(currentIndex+1)%3
+
+//         $(".slider").eq(currentIndex).fadeOut(1200)
+//         $(".slider").eq(nextIndex).fadeIn(1200)
+//         currentIndex=nextIndex
+//     },3000)
+// })
+
+
+$(function(){
+    let currentIndex=0
+    $(".slider").hide().first().show()
+
+    setInterval(function(){
+        let nextIndex=(currentIndex+1)%3
+        $(".slider").eq(currentIndex).fadeOut(1200)
+        $(".slider").eq(nextIndex).fadeIn(1200)
+
+        currentIndex=nextIndex
+    },3000)
+})
