e4a0e4c8746f72bc8958c3e5495d31a1ecf2cd6f
dddodddo <99pdh@naver.com>
2024-06-06 23:28:24 +0900

    06.06**

---
+++ 연습/자격증/(D2)서울구석구석/js/script.js
@@ -0,0 +1,12 @@
+$(function(){
+    let currentIndex=0
+    $(".img").append($(".si").eq(0).clone(true))
+
+    // setInterval(function(){
+    //     currentIndex++
+    //     $(".si").animate({marginLeft:-currentIndex * 100+"px"},600)
+    // })
+})
+
+let w = $(window).width()
+console.log(w)
