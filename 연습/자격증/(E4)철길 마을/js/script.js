b938ba27a180de0b91aed8fa9ad2f42f4d5a4ae0
flex-site <99pdh@naver.com>
2024-06-10 14:49:46 +0900

    06.10/5

--- 연습/자격증/(E4)철길 마을/js/script.js
+++ 연습/자격증/(E4)철길 마을/js/script.js
@@ -2,12 +2,13 @@ $(function(){
     let currentIndex=0
     $(".sliderWrap").append($(".img").first().clone(true))
 
-    // setInterval(function(){
-    //     $(".slider .sliderWrap").animate({marginLeft:-currentIndex*100+"%"},600)
-    //     if(currentIndex==3){
-    //         setTimeout(function(){
-    //             $(".slider .sliderWrap").animate({marginLeft:0},0) 
-    //         },700)
-    //     }
-    // },3000)
+    setInterval(function(){
+        currentIndex++
+        $(".slider .sliderWrap").animate({marginLeft:-currentIndex*100+"%"},600)
+        if(currentIndex==3){
+            setTimeout(function(){
+                $(".slider .sliderWrap").animate({marginLeft:0},0) 
+            },700)
+        }
+    },3000)
 })
