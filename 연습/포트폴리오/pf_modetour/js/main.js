8c6de7f8ee6b426d22eba961a1b516da27a057d7
flex-site <99pdh@naver.com>
2024-06-14 14:09:08 +0900

    06.14

--- 연습/포트폴리오/pf_modetour/js/main.js
+++ 연습/포트폴리오/pf_modetour/js/main.js
@@ -1,69 +1,86 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
+const lenis = new Lenis();
 
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+lenis.on("scroll", ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
 // color  #00A87F
 // color: rgb(153, 153, 153)
 
 ////////////////////////🎁search icon🎁////////////////////////
 $(function () {
-  let Icon = $(".fa-search")
+  let Icon = $(".fa-search");
   Icon.click(function () {
-    let currentColor = $(this).css('color')
-    Icon.css("color", "rgb(153, 153, 153)")
+    let currentColor = $(this).css("color");
+    Icon.css("color", "rgb(153, 153, 153)");
     if (currentColor == "rgb(153, 153, 153)") {
-      $(this).css("color", "#00A87F")
+      $(this).css("color", "#00A87F");
     }
-  })
-})
-
-
+  });
+});
 
 ////////////////////////🎁clock🎁////////////////////////
 function updateClock() {
-  var now = new Date()
+  var now = new Date();
   var hours = String(now.getHours()).padStart(2, "0");
   var minutes = String(now.getMinutes()).padStart(2, "0");
   var seconds = String(now.getSeconds()).padStart(2, "0");
 
-  document.getElementById('hoursTens').textContent = hours[0];
-  document.getElementById('hoursOnes').textContent = hours[1];
-  document.getElementById('minTens').textContent = minutes[0];
-  document.getElementById('minOnes').textContent = minutes[1];
-  document.getElementById('secTens').textContent = seconds[0];
-  document.getElementById('secOnes').textContent = seconds[1];
+  document.getElementById("hoursTens").textContent = hours[0];
+  document.getElementById("hoursOnes").textContent = hours[1];
+  document.getElementById("minTens").textContent = minutes[0];
+  document.getElementById("minOnes").textContent = minutes[1];
+  document.getElementById("secTens").textContent = seconds[0];
+  document.getElementById("secOnes").textContent = seconds[1];
 }
 setInterval(updateClock, 1000);
 
-
-
 ////////////////////////🎁mv-video🎁////////////////////////
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".mv",
-    start:"top top",
-    end:"90% top",
-    scrub:2,
-    duration:3,
-    pinSpacing: false
-  }
-})
-.to(".mv",{opacity:0.3,duration:100},5)
-.to(".mv .nav",{x:-30+"%",y:50+"%",duration:100},5)
-.to(".mv .logo img",{x:-67+"%",y:410+"%",scale:0.2,duration:100},5)
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".mv",
+      start: "top top",
+      end: "90% top",
+      scrub: 2,
+      duration: 3,
+      pinSpacing: false,
+    },
+  })
+  .to(".mv", { opacity: 0.3, duration: 100 }, 5)
+  .to(".mv .nav", { x: -30 + "%", y: 50 + "%", duration: 100 }, 5)
+  .to(
+    ".mv .logo img",
+    { x: -67 + "%", y: 410 + "%", scale: 0.2, duration: 100 },
+    5
+  );
 
 ////////////////////////🎁header🎁////////////////////////
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".section1",
-    start:"top 10%",
-    end:"top 10%",
-    scrub:2,
-    duration:10,
-    markers: true,
-    pinSpacing: false
-  }
-})
-.to(".mv video",{opacity:0})
-.to(".mv .nav",{opacity:0})
-.to(".mv .logo img",{opacity:0})
-.to(".header",{opacity:1},0.2)
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section1",
+      start: "top 10%",
+      end: "top 10%",
+      scrub: 2,
+      duration: 10,
+      markers: true,
+      pinSpacing: false,
+    },
+  })
+  .to(".mv video", { opacity: 0 })
+  .to(".mv .nav", { opacity: 0 })
+  .to(".mv .logo img", { opacity: 0 })
+  .to(".header", { opacity: 1 }, 0.2);
+
+  
+////////////////////////🎁header🎁////////////////////////
