a67843b683eed657a00572b009ccac7e2ec9314f
basic <roopre48@daum.net>
2024-07-04 20:05:42 +0900

    0704

--- 연습/포트폴리오/pf/js/main.js
+++ 연습/포트폴리오/pf/js/main.js
@@ -322,9 +322,9 @@ gsap
       markers: true,
     },
   })
-  .to(".card1",{ top: '16%',duration:3,ease: "power2.out"})
-  .to(".card2",{ top: '47%',duration:3,ease: "power2.out"},"-=2")
-  .to(".card3",{ top: '51%',duration:3,ease: "power2.out"},"-=2")
+  .to(".card1",{ top: '16%',duration:5,ease: "power2.out"})
+  .to(".card2",{ top: '47%',duration:5,ease: "power2.out"},"-=2")
+  .to(".card3",{ top: '51%',duration:5,ease: "power2.out"},"-=2")
   .fromTo(".color1",{ color:'#333',duration:3,ease: "power2.out"},{ color:'#437fc4',duration:3,ease: "power2.out"})
   .fromTo(".color2",{ color:'#333',duration:3,ease: "power2.out"},{ color:'#43c46a',duration:3,ease: "power2.out"})
   .fromTo(".color3",{ color:'#333',duration:3,ease: "power2.out"},{ color:'#ffae00',duration:3,ease: "power2.out"})
@@ -363,7 +363,8 @@ gsap
   
   .to(
     ".tiles__title", {
-      top: "-7vh",
+      color:"#333",
+      top: 0,
       duration: 2,
       ease: "power3.inOut"
     },
@@ -371,7 +372,8 @@ gsap
   )
   .to(
     ".content1", {
-      marginTop: "20vh",
+      marginTop: "30vh",
+      color:"#fff",
       duration: 2,
       ease: "power3.inOut"
     },
@@ -404,5 +406,79 @@ toggleGrayscale();
 // Set interval to toggle grayscale every 3 seconds (adjust interval as needed)
 setInterval(toggleGrayscale, 3000);
 
+
+////////////////////////////////////////////////////////////////////
+window.addEventListener("DOMContentLoaded",() => {
+	const ctl = new CollapsibleTimeline("#timeline");
+});
+
+class CollapsibleTimeline {
+	constructor(el) {
+		this.el = document.querySelector(el);
+
+		this.init();
+	}
+	init() {
+		this.el?.addEventListener("click",this.itemAction.bind(this));
+	}
+	animateItemAction(button,ctrld,contentHeight,shouldCollapse) {
+		const expandedClass = "timeline__item-body--expanded";
+		const animOptions = {
+			duration: 300,
+			easing: "cubic-bezier(0.65,0,0.35,1)"
+		};
+
+		if (shouldCollapse) {
+			button.ariaExpanded = "false";
+			ctrld.ariaHidden = "true";
+			ctrld.classList.remove(expandedClass);
+			animOptions.duration *= 2;
+			this.animation = ctrld.animate([
+				{ height: `${contentHeight}px` },
+				{ height: `${contentHeight}px` },
+				{ height: "0px" }
+			],animOptions);
+		} else {
+			button.ariaExpanded = "true";
+			ctrld.ariaHidden = "false";
+			ctrld.classList.add(expandedClass);
+			this.animation = ctrld.animate([
+				{ height: "0px" },
+				{ height: `${contentHeight}px` }
+			],animOptions);
+		}
+	}
+	itemAction(e) {
+		const { target } = e;
+		const action = target?.getAttribute("data-action");
+		const item = target?.getAttribute("data-item");
+
+		if (action) {
+			const targetExpanded = action === "expand" ? "false" : "true";
+			const buttons = Array.from(this.el?.querySelectorAll(`[aria-expanded="${targetExpanded}"]`));
+			const wasExpanded = action === "collapse";
+
+			for (let button of buttons) {
+				const buttonID = button.getAttribute("data-item");
+				const ctrld = this.el?.querySelector(`#item${buttonID}-ctrld`);
+				const contentHeight = ctrld.firstElementChild?.offsetHeight;
+
+				this.animateItemAction(button,ctrld,contentHeight,wasExpanded);
+			}
+
+		} else if (item) {
+			const button = this.el?.querySelector(`[data-item="${item}"]`);
+			const expanded = button?.getAttribute("aria-expanded");
+
+			if (!expanded) return;
+
+			const wasExpanded = expanded === "true";
+			const ctrld = this.el?.querySelector(`#item${item}-ctrld`);
+			const contentHeight = ctrld.firstElementChild?.offsetHeight;
+
+			this.animateItemAction(button,ctrld,contentHeight,wasExpanded);
+		}
+	}
+}
 ////////////////////////////////////////////////////////////////////
 
