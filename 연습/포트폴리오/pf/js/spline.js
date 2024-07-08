8a54b47cdf2da0c30b47360394caefeb4c57fc45
basic <roopre48@daum.net>
2024-07-08 17:55:23 +0900

    .

--- 연습/포트폴리오/pf/js/spline.js
+++ 연습/포트폴리오/pf/js/spline.js
@@ -13,6 +13,12 @@ const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app
 .load('https://prod.spline.design/ejt5-ivKGLXHUSqd/scene.splinecode') //가져왔을 때
-.then(()=>{//load가 성공했다면 할 일
+.then(()=>{})
+
+// const canvas2 = document.getElementById('canvas3d2');
+// const app2 = new Application(canvas2);
+// app2
+// .load('https://prod.spline.design/t75vY61hoi6b5a3K/scene.splinecode') //가져왔을 때
+// .then(()=>{//load가 성공했다면 할 일
     
-  })
+// })
