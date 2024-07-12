4a8ab55bbc54467eecc22ece1565d5212a74cb32
git <roopre48@daum.net>
2024-07-12 13:36:24 +0900

    0712

--- 연습/포트폴리오/pf/js/main.js
+++ 연습/포트폴리오/pf/js/main.js
@@ -1020,9 +1020,6 @@ window.onpointermove = event => {
 }
 
 ////////////////////////////////////////////////////
-// Split text by lines and chars
-new SplitText('[split-lines]', { type: 'lines', linesClass: "split-line" })
-
 document.addEventListener('DOMContentLoaded', () => {
   const aboutHover = document.querySelectorAll('.about-hover');
 
@@ -1043,6 +1040,7 @@ document.addEventListener('DOMContentLoaded', () => {
       .timeline({
         defaults: {
           ease: 'none',
+          duration:2.5,
         },
       })
       .to(".about-subtitle", {
@@ -1055,16 +1053,7 @@ document.addEventListener('DOMContentLoaded', () => {
         ease: "none",
         duration: 0
       }, '<')
-      .fromTo(".about-text .split-line", {
-        y: '100%'
-      }, {
-        y: 0,
-        opacity: 1,
-        ease: "sine.out",
-        transformOrigin: "top",
-        stagger: 0.1,
-        duration: 1.2
-      }, '<0.4')
+
       .to(".about-down", {
         opacity: 1,
         ease: "sine.out",
@@ -1072,9 +1061,6 @@ document.addEventListener('DOMContentLoaded', () => {
         stagger: 0.1,
         duration: 0.4
       }, '>')
-      .call(() => {
-        document.querySelector('body').classList.remove('overflow-hidden');
-      });
   })
 });
 ////////////////////////////////////////////////////
