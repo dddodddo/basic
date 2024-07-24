ef669e7754b98df0cfb8fc3619ad59a6d7c41896
basic <roopre48@daum.net>
2024-07-24 14:44:25 +0900

    3

--- reactApp/같은ID로 여러경로설정/src/pages/VideoDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-function VideoDetail() {
-  const {videoId}=useParams();
-  return <div>VideoDetail{videoId}</div>;
-}
-
-export default VideoDetail;
